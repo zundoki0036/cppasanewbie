c3d6945122e020e7df6538f6c0f186b0e5abb672
Zundoki <92425732+zundoki0036@users.noreply.github.com>
2025-11-08 10:53:48 +0700

    Update bai1.cpp

--- #3 Mathmethical Basics/bai1.cpp
+++ #3 Mathmethical Basics/bai1.cpp
@@ -8,8 +8,19 @@ int ucln(int a, int b){
     return ucln(a, b - a);
 }
 
+//thuat toan neu bi tran so
+long long ucln(long long a, long long b) {
+    while (b != 0) {
+        long long r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
 int main (){
     int a,b; cin >> a >> b;
     cout << "UCLN: " << ucln (a,b) << endl;
     cout << "BCNN: " << (a/ucln(a,b))*b;
+
 }
