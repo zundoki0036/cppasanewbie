ba82863d1ffb1f276a4c1251e2ed24ed29cd7543
Zundoki <92425732+zundoki0036@users.noreply.github.com>
2025-12-04 07:48:49 +0700

    Update bai3.cpp

--- #7 Array/bai3.cpp
+++ #7 Array/bai3.cpp
@@ -25,3 +25,9 @@ void solve(int *arr,int n) {
     }
     cout << ml;
 }
+
+//thuật toán: duyệt 2 phần tử liền kề trong mảng, nếu nó trùng giá trị hoặc kém hơn 1 thì count + 1 lên
+//không được vừa đặt > < 1 đơn vị, vì sẽ có lúc bị trùng và nảy sinh trường hợp
+// 3 4 2 (mà 4 lệch 2 so với số 2) nên cứ duyệt qua 1 lần là ổn rồi
+// sau đó ở mỗi phần tử sẽ có số count nhất định, lưu vào biến toàn hàm main cố định là ml (max length của chuỗi), và dùng nó để so sánh
+// chuỗi có nhiều phần tử nhất ở mỗi phần tử vòng lặp chạy qua
