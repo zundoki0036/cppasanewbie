2091134bac04ccc03e877720ec3c40843a05d9c2
Zundoki <92425732+zundoki0036@users.noreply.github.com>
2025-12-10 11:34:19 +0700

    Hoàn thiện bài xếp đề

--- #1 Practice - Sep 29 2025/phatde.cpp
+++ #1 Practice - Sep 29 2025/phatde.cpp
@@ -1,6 +1,41 @@
-# include <iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-  return 0;
-}
+    int n,k,p,q,soghe;
+    cin >> n >> k >> p >> q;
+    soghe = (p - 1) * 2 + q; // tìm số ghế của alice
+
+    int ghetruoc = soghe - k; // khả năng bob gần alice xếp trên
+    int ghesau = soghe + k; // khả năng bob gần alice xếp dưới
+    int ghebob = -1; // Khởi tạo là -1 (chưa tìm thấy)
+
+    bool valid_truoc = (ghetruoc >= 1); 
+    bool valid_sau = (ghesau <= n);
+    // nếu ghế trước = 0 (trùng) hoặc ghế sau vượt số ghế của lớp thì out
+
+    if (valid_truoc && valid_sau) {
+        int row_truoc = (ghetruoc + 1) / 2; //phân tích theo hàng
+        int row_sau   = (ghesau   + 1) / 2;
+        int d_truoc = abs(row_truoc - p); 
+        int d_sau   = abs(row_sau   - p);
+
+        if (d_truoc <= d_sau) ghebob = ghetruoc;
+        else ghebob = ghesau;
+    }
+    else if (valid_truoc) {
+        ghebob = ghetruoc;
+    }
+    else if (valid_sau) {
+        ghebob = ghesau;
+    }
+
+    // Kiểm tra kết quả cuối cùng
+    if (ghebob == -1) {
+        cout << ghebob << endl;
+    } else {
+        int u = (ghebob + 1) / 2; // đưa số hàng về 
+        int v = (ghebob % 2 == 1) ? 1 : 2; //phân tích theo cột bằng phần dư phép chia lấy u
+        cout << u << " " << v << endl;
+    }
